300445bd2cfa726af7c5eb8b881411eb17cbe5f3
elifboluk <elifbolukk@gmail.com>
2022-09-21 18:08:55 +0300

    React

--- reactclient/src/App.js
+++ reactclient/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row min-vh-100">
+        <div className="col d-flex flex-column justify-content-center align-items-center">
+          <h1>Hello Bootstrap!</h1>
+
+          {renderOrderDetailsTable()}
+        </div>
+      </div>      
+    </div>
+  );
+
+function renderOrderDetailsTable(){
+  return (
+    <div className="table-responsive mt-15">
+      <table className="table table-bordered border-dark">
+        <thead>
+          <tr>
+            <th scope="col">OrderDetailId(PK)</th>
+            <th scope="col">Total</th> 
+            <th scope="col">ProductId</th>
+            <th scope="col">Product</th>
+            <th scope="col">OrderId</th>
+            <th scope="col">Order</th>
+            <th scope="col">CRUD Operations</th>            
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope='row'>1</th>
+            <td>OrderDetailId 1 Total</td>
+            <td>OrderDetailId 1 ProductId</td>
+            <td>OrderDetailId 1 Product</td>
+            <td>OrderDetailId 1 OrderId</td>
+            <td>OrderDetailId 1 Order</td>
+            <td>
+              <button className="btn btn-dark btn-lg active mx-9 my-3">Update</button>
+              <button className="btn btn-danger btn-lg active">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+}
 
-export default App;
+
